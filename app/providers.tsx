3af2e7c28a5940729ccbaf817ead236efd16048a
RanSolo <ransolo@me.com>
2024-05-19 16:01:36 -0500

    remove-base-path

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,7 @@ import { ModalProvider } from "@/components/modal/provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider basePath="/">
+    <SessionProvider>
       <Toaster className="dark:hidden" />
       <Toaster theme="dark" className="hidden dark:block" />
       <ModalProvider>{children}</ModalProvider>
