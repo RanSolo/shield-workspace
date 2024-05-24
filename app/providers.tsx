078a09c5bdca4112471d837886bde5b8e54b8e0e
RanSolo <ransolo@me.com>
2024-05-23 19:16:42 -0500

    remove-placeholder

--- app/providers.tsx
+++ app/providers.tsx
@@ -12,7 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <Script
         defer
         src="https://unpkg.com/@tinybirdco/flock.js"
-        data-token="p.eyJ1IjogIjUyYWZlMmM1LTEwNzYtNGU5Mi1iMmY4LTA0ZWVhN2VkYjVjYyIsICJpZCI6ICI4NjBhODViMi1jYTgxLTQ5N2UtOGJiYi01MzhmOWM3ODg1OWIiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.uRBA5Uo5eFHpN8XX9lIqAqxfpfX9uRU2Odc15-J0iZwYOUR_TRACKER_TOKEN" />
+        data-token="p.eyJ1IjogIjUyYWZlMmM1LTEwNzYtNGU5Mi1iMmY4LTA0ZWVhN2VkYjVjYyIsICJpZCI6ICI4NjBhODViMi1jYTgxLTQ5N2UtOGJiYi01MzhmOWM3ODg1OWIiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.uRBA5Uo5eFHpN8XX9lIqAqxfpfX9uRU2Odc15-J0iZw" />
     <SessionProvider>
       <Toaster className="dark:hidden" />
       <Toaster theme="dark" className="hidden dark:block" />
