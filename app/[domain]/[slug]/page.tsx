783f6e51f94f674ed7602f5b0c1bfc251e07f4ce
RanSolo <ransolo@me.com>
2024-06-24 15:33:23 -0500

    clean up

--- app/[domain]/[slug]/page.tsx
+++ app/[domain]/[slug]/page.tsx
@@ -37,12 +37,12 @@ export async function generateMetadata({
       creator: "@vercel",
     },
     // Optional: Set canonical URL to custom domain if it exists
-    // ...(params.domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
-    //   siteData.customDomain && {
-    //     alternates: {
-    //       canonical: `https://${siteData.customDomain}/${params.slug}`,
-    //     },
-    //   }),
+    ...(params.domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
+      siteData.customDomain && {
+        alternates: {
+          canonical: `https://${siteData.customDomain}/${params.slug}`,
+        },
+      }),
   };
 }
 
@@ -97,54 +97,21 @@ export default async function SitePostPage({
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-        <div className="m-auto w-full text-center md:w-7/12">
-          <p className="m-auto my-5 w-10/12 text-sm font-light text-stone-500 dark:text-stone-400 md:text-base">
-            {toDateString(data.createdAt)}
-          </p>
-          <h1 className="mb-10 font-title text-3xl font-bold text-stone-800 dark:text-white md:text-6xl">
+        <div className="w-full m-auto text-center md:w-7/12">
+          <h1 className="mb-10 text-3xl font-bold font-title text-stone-800 dark:text-white md:text-6xl">
             {data.title}
           </h1>
-          <p className="text-md m-auto w-10/12 text-stone-600 dark:text-stone-400 md:text-lg">
+          <p className="w-10/12 m-auto text-md text-stone-600 dark:text-stone-400 md:text-lg">
             {data.description}
           </p>
         </div>
-        <a
-          // if you are using Github OAuth, you can get rid of the Twitter option
-          href={
-            data.site?.user?.username
-              ? `https://twitter.com/${data.site.user.username}`
-              : `https://github.com/${data.site?.user?.gh_username}`
-          }
-          rel="noreferrer"
-          target="_blank"
-        >
-          <div className="my-8">
-            <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle md:h-12 md:w-12">
-              {data.site?.user?.image ? (
-                <BlurImage
-                  alt={data.site?.user?.name ?? "User Avatar"}
-                  height={80}
-                  src={data.site.user.image}
-                  width={80}
-                />
-              ) : (
-                <div className="absolute flex h-full w-full select-none items-center justify-center bg-stone-100 text-4xl text-stone-500">
-                  ?
-                </div>
-              )}
-            </div>
-            <div className="text-md ml-3 inline-block align-middle dark:text-white md:text-lg">
-              by <span className="font-semibold">{data.site?.user?.name}</span>
-            </div>
-          </div>
-        </a>
       </div>
-      <div className="relative m-auto mb-10 h-80 w-full max-w-screen-lg overflow-hidden md:mb-20 md:h-150 md:w-5/6 md:rounded-2xl lg:w-2/3">
+      <div className="relative w-full max-w-screen-lg m-auto mb-10 overflow-hidden h-80 md:mb-20 md:h-150 md:w-5/6 md:rounded-2xl lg:w-2/3">
         <BlurImage
           alt={data.title ?? "Post image"}
           width={1200}
           height={630}
-          className="h-full w-full object-cover"
+          className="object-cover w-full h-full"
           placeholder="blur"
           blurDataURL={data.imageBlurhash ?? placeholderBlurhash}
           src={data.image ?? "/placeholder.png"}
@@ -154,22 +121,22 @@ export default async function SitePostPage({
       <MDX source={data.mdxSource} />
 
       {data.adjacentPosts.length > 0 && (
-        <div className="relative mb-20 mt-10 sm:mt-20">
+        <div className="relative mt-10 mb-20 sm:mt-20">
           <div
-            className="absolute inset-0 flex items-center"
+            className="absolute inset-0 flex flex-row items-center"
             aria-hidden="true"
           >
             <div className="w-full border-t border-stone-300 dark:border-stone-700" />
           </div>
           <div className="relative flex justify-center">
-            <span className="bg-white px-2 text-sm text-stone-500 dark:bg-black dark:text-stone-400">
+            <span className="px-2 text-sm bg-white text-stone-500 dark:bg-black dark:text-stone-400">
               Continue Reading
             </span>
           </div>
         </div>
       )}
       {data.adjacentPosts && (
-        <div className="mx-5 mb-20 grid max-w-screen-xl grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:mx-auto xl:grid-cols-3">
+        <div className="grid max-w-screen-xl grid-cols-1 mx-5 mb-20 gap-x-4 gap-y-8 md:grid-cols-2 xl:mx-auto xl:grid-cols-3">
           {data.adjacentPosts.map((data: any, index: number) => (
             <BlogCard key={index} data={data} />
           ))}
--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
-import BlurImage from "@/components/blur-image";
 import { placeholderBlurhash, toDateString } from "@/lib/utils";
-import BlogCard from "@/components/blog-card";
 import { getPostsForSite, getSiteData } from "@/lib/fetchers";
 import Image from "next/image";
 import Iframe from 'react-iframe'
@@ -53,7 +51,7 @@ export default async function SiteHomePage({
     <>
       <div className="w-full mt-20 mb-20">
         <div className="relative w-full pb-5 h-fit">
-          <div className="relative w-full h-full mx-auto overflow-hidden group ">
+          <div className="relative w-full h-full mx-auto overflow-hidden group">
             <Image
               alt='band photo'
               blurDataURL={placeholderBlurhash}
@@ -65,96 +63,29 @@ export default async function SiteHomePage({
             />
           </div>
         </div>
-        {posts.length > 0 ? (
           <div className="w-1/2 max-w-screen-xl mx-auto md:mb-28 lg:w-5/6">
-            <Link href={`/${posts[0].slug}`}>
-              <div className="relative w-full mx-auto overflow-hidden group h-80 sm:h-150 lg:rounded-xl">
-                <BlurImage
-                  alt={posts[0].title ?? ""}
-                  blurDataURL={posts[0].imageBlurhash ?? placeholderBlurhash}
-                  className="object-cover w-full h-full group-hover:scale-105 group-hover:duration-300"
-                  width={1300}
-                  height={630}
-                  placeholder="blur"
-                  src={posts[0].image ?? "/placeholder.png"}
-                />
-              </div>
+          {posts.map(post => (
+
+            <Link  key={post.slug} href={`/${post.slug}`}>
               <div className="w-5/6 mx-auto mt-10 lg:w-full">
                 <h2 className="my-10 text-4xl font-title dark:text-white md:text-6xl">
-                  {posts[0].title}
+                  {post.title}
                 </h2>
-                <p className="w-full text-base dark:text-white md:text-lg lg:w-2/3">
-                  {posts[0].description}
-                </p>
-                <div className="flex items-center justify-start w-full space-x-4">
-                  <div className="relative flex-none w-8 h-8 overflow-hidden rounded-full">
-                    {data.user?.image ? (
-                      <BlurImage
-                        alt={data.user?.name ?? "User Avatar"}
-                        width={100}
-                        height={100}
-                        className="object-cover w-full h-full"
-                        src={data.user?.image}
-                      />
-                    ) : (
-                      <div className="absolute flex items-center justify-center w-full h-full text-4xl select-none bg-stone-100 text-stone-500">
-                        ?
-                      </div>
-                    )}
-                  </div>
-                  <p className="inline-block ml-3 text-sm font-semibold align-middle whitespace-nowrap dark:text-white md:text-base">
-                    {data.user?.name}
-                  </p>
-                  <div className="h-6 border-l border-stone-600 dark:border-stone-400" />
-                  <p className="w-10/12 m-auto my-5 text-sm font-light text-stone-500 dark:text-stone-400 md:text-base">
-                    {toDateString(posts[0].createdAt)}
-                  </p>
-                </div>
               </div>
             </Link>
-          </div>
-        ) : (
-          <div className="flex flex-col items-center justify-center py-20">
-            <Image
-              alt="missing post"
-              src="https://illustrations.popsy.co/gray/success.svg"
-              width={400}
-              height={400}
-              className="dark:hidden"
-            />
-            <Image
-              alt="missing post"
-              src="https://illustrations.popsy.co/white/success.svg"
-              width={400}
-              height={400}
-              className="hidden dark:block"
-            />
-            <p className="text-2xl font-title text-stone-600 dark:text-stone-400">
-              No posts yet.
-            </p>
-          </div>
-        )}
-      </div>
+          )
 
-      {posts.length > 1 && (
-        <div className="max-w-screen-xl mx-5 mb-20 lg:mx-24 2xl:mx-auto">
-          <h2 className="mb-10 text-4xl font-title dark:text-white md:text-5xl">
-            More stories
-          </h2>
-          <div className="grid w-full grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:grid-cols-3">
-            {posts.slice(1).map((metadata: any, index: number) => (
-              <BlogCard key={index} data={metadata} />
-            ))}
+          )}
           </div>
-        </div>
-      )}
-                <Iframe url={data.featuredEmbed as string}
-            width="100%"
-            height="1000"
-            id=""
-            className=""
-            display="block"
-            position="relative"/>
+          
+      </div>
+      <Iframe url={data.featuredEmbed as string}
+        width="100%"
+        height="1000"
+        id=""
+        className=""
+        display="block"
+        position="relative"/>
     </>
   );
 }
